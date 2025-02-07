4247416f984fab7d634287df4f0a4f7f81835899
victorfranca03 <ga.victorh@gmail.com>
2025-02-06 23:29:55 -0300

    wip

--- Aula4JS/aula4.js
+++ Aula4JS/aula4.js
@@ -1,11 +1,9 @@
 var nome = "Victor";
-var idade = 22;
+var idade = prompt("Qual a sua idade?");
 var altura = 1.90;
 
-var resposta = prompt("Qual o seu nome?");
 
 console.log (nome);
-console.log (idade);
 console.log (altura);
 
 //Esá são minhas informações basicas
